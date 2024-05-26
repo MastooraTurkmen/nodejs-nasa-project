3266b57d15a982a55b2a70c729464946c016695b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-26 20:36:45 +0430

    set up

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
+
+const planetsRouter = require('./routes/planets/planest.router');
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.use(planetsRouter());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
+
+module.exports = app;
---
+++ server/src/models/planets.modal.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path')
+const parse = require('csv-parse');
+
+const habitablePlanets = [];
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', (data) => {
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err)
+            })
+            .on('end', () => {
+                console.log(`${habitablePlanets.length} habitable planets found!`);
+                resolve();
+            });
+    })
+}
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+}
