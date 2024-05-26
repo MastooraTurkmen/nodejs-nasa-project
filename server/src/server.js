e42e36f765c2f232f952044b0e00efd8ed66d3e0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-26 20:34:46 +0430

    server js

---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const app = require('./app');
+
+const { loadPlanetsData } = require('./models/planets.modal');
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}..`);
+    });
+}
+
+startServer();
