4168a8d5af584bc42c4f4be443fdeb0864548826
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 18:43:51 +0430

    planets

--- server/src/models/planets.modal.js
+++ server/src/models/planets.modal.js
@@ -27,7 +27,11 @@ function loadPlanetsData() {
     })
 }
 
+function getAllPlanets() {
+    return habitablePlanets;
+}
+
 module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets,
+    habitablePlanets
 }
