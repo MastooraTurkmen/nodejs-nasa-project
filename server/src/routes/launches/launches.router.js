fe65d4cdb4c6eda786e2ee51edaccdfbaed8be5c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 18:28:17 +0430

    set up router

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { httpGetAllLaunches } = require('./launches.controller')
+
+const launchesRouter = express.Router();
+
+launchesRouter.get('/launches', httpGetAllLaunches);
+
+module.exports = launchesRouter;
