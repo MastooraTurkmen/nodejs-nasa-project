a596a8dacc726394f98b9578066db9c457c4ef90
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 20:29:15 +0430

    post launches creating

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
-const { httpGetAllLaunches } = require('./launches.controller')
+const { httpGetAllLaunches, httpAddNewLaunch } = require('./launches.controller')
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
 
 module.exports = launchesRouter;
