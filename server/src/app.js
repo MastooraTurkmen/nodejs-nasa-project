1de264fb5d95eb53af240cca49ad5877b7af26c1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 18:28:10 +0430

    set up

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planest.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -16,8 +17,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 
-app.use(planetsRouter());
-app.get('/', (req, res) => {
+app.use(planetsRouter);
+app.use(launchesRouter)
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
