0ffaaaa51de37120a13b731b311f06cfab2a2a21
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 20:29:08 +0430

    post launches creating

--- server/src/models/launches.modal.js
+++ server/src/models/launches.modal.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -17,6 +19,17 @@ function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(launch.flightNumber, Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customer: ['Zero to Mastery', 'NASA'],
+        flightNumber: latestFlightNumber,
+    }))
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch
 }
