3a333badd98bfa9ebececa8be8231d02feb72afb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 18:27:03 +0430

    launches modal

---
+++ server/src/models/launches.modal.js
@@ -0,0 +1,22 @@
+const launches = new Map();
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-422 b',
+    customer: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true,
+}
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+module.exports = {
+    getAllLaunches,
+}
