3e7fa3667ca232d0bf91aff3a18a40b797228abc
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-27 20:29:11 +0430

    post launches creating

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,19 @@
-const { getAllLaunches } = require('../../models/launches.modal')
+const { getAllLaunches, addNewLaunch } = require('../../models/launches.modal')
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+    const launch = req.body;
+
+    launch.launchDate = new Date(launch.launchDate);
+
+    addNewLaunch(launch);
+    return res.status(201).json(launch)
+}
+
 module.exports = {
     httpGetAllLaunches,
+    httpAddNewLaunch
 }
