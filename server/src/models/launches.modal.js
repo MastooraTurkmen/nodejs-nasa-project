6a54df0d8f6c89fdce8ca798729744fd0d9de7d9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 09:36:14 +0430

    delete launches

--- server/src/models/launches.modal.js
+++ server/src/models/launches.modal.js
@@ -15,6 +15,10 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existLaunchesWithId(launchId) {
+    return launches.has(launchId)
+}
+
 function getAllLaunches() {
     return Array.from(launches.values());
 }
@@ -29,7 +33,16 @@ function addNewLaunch(launch) {
     }))
 }
 
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId)
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
 module.exports = {
+    existLaunchesWithId,
     getAllLaunches,
-    addNewLaunch
+    addNewLaunch,
+    abortLaunchById
 }
