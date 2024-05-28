c1954d9591fc7fa0b1e190c15d0567f91b754541
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 10:20:23 +0430

    jest test

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('Test GET /launches', () => {
+    test("It should respond with 200 success", () => {
+        const response = 200;
+        expect(response).toBe(200);
+    });
+});
+
+describe('Test POST /launch', () => {
+    test("It should respond with 200 success", () => {
+
+    })
+    test('It should catch missing required propertires', () => { });
+    test('It should catch invalid dates', () => { });
+})
