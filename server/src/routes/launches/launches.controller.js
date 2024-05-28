b03c5ad2f1df9e612dd65abb2ccff2f1f0debace
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 07:42:42 +0430

    post launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,7 +7,18 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        return res.status(400).json({
+            error: "Missing required launch property",
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        })
+    }
 
     addNewLaunch(launch);
     return res.status(201).json(launch)
