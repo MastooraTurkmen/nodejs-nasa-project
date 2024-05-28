2330631ca5cd6e7759a273babf6abc9296515557
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 13:56:53 +0430

    jest test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,10 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-    test("It should respond with 200 success", () => {
-        const response = 200;
-        expect(response).toBe(200);
+    test("It should respond with 200 success", async () => {
+        const response = await request(app).get('/launches');
+        expect(response.statusCode).toBe(200);
     });
 });
 
