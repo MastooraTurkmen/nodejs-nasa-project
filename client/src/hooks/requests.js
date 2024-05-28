67da94fd8d8ba1a28ca092275a47ccd75f74b922
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 09:35:55 +0430

    delete launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -32,9 +32,18 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'delete',
+    })
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false
+    }
+  }
 }
 
 export {
