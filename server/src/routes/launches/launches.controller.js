99a93754a12eae6b6136341743bc6dafc925e4f1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 09:36:08 +0430

    delete launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.modal')
+const { getAllLaunches, addNewLaunch, existLaunchesWithId } = require('../../models/launches.modal')
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -24,7 +24,21 @@ function httpAddNewLaunch(req, res) {
     return res.status(201).json(launch)
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = req.params.id;
+
+    if (!existLaunchesWithId(launchId)) {
+        return res.status(404).json({
+            error: 'Launch not found',
+        })
+    }
+
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
-    httpAddNewLaunch
+    httpAddNewLaunch,
+    httpAbortLaunch
 }
