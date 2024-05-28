2e111888920b19c7588f1ac3b7079269d53d4a75
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-28 09:56:13 +0430

    delete launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -25,7 +25,7 @@ function httpAddNewLaunch(req, res) {
 }
 
 function httpAbortLaunch(req, res) {
-    const launchId = req.params.id;
+    const launchId = Number(req.params.id);
 
     if (!existLaunchesWithId(launchId)) {
         return res.status(404).json({
