da4fc3b055ecdb11570f83c6d743b800d59c54b5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-30 20:23:58 +0430

    creat mongoose schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const { loadPlanetsData } = require('./models/planets.modal');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:O5rAFoDGUu2w8kVW@cluster0.0uht7wb.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL)
     await loadPlanetsData();
 
     server.listen(PORT, () => {
