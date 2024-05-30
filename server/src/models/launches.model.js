1cc288f793e9cb3b6fdf2ea90a489d661c919ef4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-30 20:24:02 +0430

    creat mongoose schema

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+        default: 100,
+        min: 100,
+        max: 999,
+    },
+    launchDate: {
+        type: Date,
+        required: true,
+    },
+    mission: {
+        type: String,
+        required: true,
+    },
+    rocket: {
+        type: String,
+        required: true,
+    },
+    target: {
+        type: mongoose.ObjectId,
+        ref: 'Planet'
+    },
+    customers: [String],
+    upcoming: {
+        type: Boolean,
+        required: true,
+    },
+    success: {
+        type: Boolean,
+        required: true,
+        default: true,
+    }
+})
