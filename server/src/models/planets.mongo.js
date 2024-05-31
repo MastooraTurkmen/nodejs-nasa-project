ad8c870b519b7dab9168ee89caf2a26ff34e2149
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-31 08:54:04 +0430

    creat mongoose schema

--- server/src/models/planets.mongo.js
+++ server/src/models/planets.mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const launchesSchema = mongoose.Schema({
+const planetSchema = mongoose.Schema({
     keplerName: {
         type: String,
         required: true,
     }
-})
+});
+
+module.exports = mongoose.model('Planet', planetSchema)
