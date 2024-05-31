5f27ec0e16cf6f75ad65cb83628c811b43cbde9f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-31 19:52:38 +0430

    launches controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,7 +1,7 @@
-const { getAllLaunches, addNewLaunch, existLaunchesWithId } = require('../../models/launches.modal')
+const { getAllLaunches, addNewLaunch, existLaunchesWithId } = require('../../models/launches.model')
 
-function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req, res) {
+    return res.status(200).json(await  getAllLaunches());
 }
 
 function httpAddNewLaunch(req, res) {
