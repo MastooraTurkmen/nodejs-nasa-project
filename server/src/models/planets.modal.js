6fdf58649ce7d4fb32e4136c78c74e9626343ea8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-05-31 19:52:27 +0430

    planet modal

--- server/src/models/planets.modal.js
+++ server/src/models/planets.modal.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 const path = require('path')
 const { parse } = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
 
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
@@ -11,27 +19,46 @@ function loadPlanetsData() {
                 comment: '#',
                 columns: true,
             }))
-            .on('data', (data) => {
-                if (data) {
-                    habitablePlanets.push(data);
+            .on('data', async (data) => {
+                if (isHabitablePlanet(data)) {
+                    savePlanet(data)
                 }
             })
             .on('error', (err) => {
                 console.log(err);
                 reject(err)
             })
-            .on('end', () => {
+            .on('end', async () => {
+                const countPlanetsFound = (await getAllPlanets()).length;
                 console.log(`${habitablePlanets.length} habitable planets found!`);
                 resolve();
             });
     })
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({}, {
+        '_id': 0,
+        '__v': 0,
+
+    });
+}
+
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name,
+        }, {
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true,
+        })
+    } catch (error) {
+        console.error(`Could not save planet ${error}`);
+    }
 }
 
 module.exports = {
     loadPlanetsData,
-    habitablePlanets
+    getAllPlanets
 }
