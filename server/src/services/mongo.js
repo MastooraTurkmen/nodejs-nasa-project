560db1a7fc1a1637d5770a54bbfb0da8b4331968
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-01 21:12:55 +0430

    mongo

---
+++ server/src/services/mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const MONGO_URL = 'mongodb+srv://nasa-api:O5rAFoDGUu2w8kVW@cluster0.0uht7wb.mongodb.net/nasa?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready');
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL)
+}
+
+module.exports = {
+    mongoConnect
+}
