9287131bde1d3f5692d53445fba5b279ff84068c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-02 17:32:27 +0430

    api

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,7 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan')
-
-const planetsRouter = require('./routes/planets/planest.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const api = require('./routes/api')
 
 const app = express();
 
@@ -15,10 +13,8 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use('/v1', api)
 
-
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter)
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
