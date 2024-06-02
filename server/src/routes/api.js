ee664ff2787d0486a19199868b86306c676dbdeb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-02 17:32:31 +0430

    api

---
+++ server/src/routes/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const planetsRouter = require('./planets/planest.router');
+const launchesRouter = require('./launches/launches.router');
+
+const api = express.Router();
+
+api.use('/v1/planets', planetsRouter);
+api.use('/v1/launches', launchesRouter);
+
+module.exports = api;
