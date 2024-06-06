cb54d1b35b0b52f8562b55da72a9acd8b04ea859
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-06 18:48:54 +0430

    launches update

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
-const { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch } = require('./launches.controller')
+const {
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch,
+} = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
-launchesRouter.delete('/:id', httpAbortLaunch)
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
