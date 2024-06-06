e1517f8872d28b968ae47cd516e3c8df80244cbe
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-06 18:49:03 +0430

    update

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,24 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan')
-const api = require('./routes/api')
+const morgan = require('morgan');
+
+const api = require('./routes/api');
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
-
 app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use('/v1', api)
+
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
