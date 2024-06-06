30f0140780f2579686f977f070367cd55c232fd8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-06 18:48:49 +0430

    launches update

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,13 @@
-const { getAllLaunches, scheduleNewLaunch, existLaunchesWithId } = require('../../models/launches.model')
+const {
+    getAllLaunches,
+    scheduleNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
+} = require('../../models/launches.model');
 
-const { getPagination } = require('../../services/query')
+const {
+    getPagination,
+} = require('../../services/query');
 
 async function httpGetAllLaunches(req, res) {
     const { skip, limit } = getPagination(req.query);
@@ -11,39 +18,41 @@ async function httpGetAllLaunches(req, res) {
 async function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
-    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.target) {
         return res.status(400).json({
-            error: "Missing required launch property",
-        })
+            error: 'Missing required launch property',
+        });
     }
 
     launch.launchDate = new Date(launch.launchDate);
     if (isNaN(launch.launchDate)) {
         return res.status(400).json({
             error: 'Invalid launch date',
-        })
+        });
     }
 
     await scheduleNewLaunch(launch);
-    return res.status(201).json(launch)
+    return res.status(201).json(launch);
 }
 
 async function httpAbortLaunch(req, res) {
     const launchId = Number(req.params.id);
 
-    const existsLaunch = await existLaunchesWithId(launchId)
+    const existsLaunch = await existsLaunchWithId(launchId);
     if (!existsLaunch) {
         return res.status(404).json({
             error: 'Launch not found',
-        })
+        });
     }
 
     const aborted = await abortLaunchById(launchId);
     if (!aborted) {
         return res.status(400).json({
             error: 'Launch not aborted',
-        })
+        });
     }
+
     return res.status(200).json({
         ok: true,
     });
@@ -52,5 +61,5 @@ async function httpAbortLaunch(req, res) {
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
-    httpAbortLaunch
-}
+    httpAbortLaunch,
+};
